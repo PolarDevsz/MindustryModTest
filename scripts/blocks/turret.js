5917a9a360368aeba6fd3e2ac54203d4d950e118
Devsz <166476627+PolarDevsz@users.noreply.github.com>
2025-03-08 19:50:20 +0700

    Update turret.js

--- scripts/blocks/turret.js
+++ scripts/blocks/turret.js
@@ -1,78 +1,49 @@
+const turret = extend(LaserTurret, "Wispion", {
+            init() {
+                this.ammo(
+                    new ContinuousLaserBulletType(78, 200, {
+                        hitEffect: Fx.hitwispion,
+                        hitColor: Pal.wispionHit,
+                        status: StatusEffects.melting,
+                        drawSize: 420,
+                        timescaleDamage: true,
+                        incendChance: 0.4,
+                        incendSpread: 5,
+                        incendAmount: 1,
+                        ammoMultiplier: 1
+                    })
+                );
+                this.super$init();
+            },
+            icons() {
+                return [
+                    Core.atlas.find("block-3"),
+                    Core.atlas.find(this.name)
+                ];
+            }
+        });
 
-const energyBall = extend(BasicBulletType,{});
-energyBall.shootEffect = Fx.none;
-energyBall.smokeEffect = Fx.none;
-energyBall.speed = 10;
-energyBall.lifetime = 100;
-energyBall.damage = 999999999;
-
-const c = extend(BasicBulletType,{});
-c.shootEffect = Fx.none;
-c.smokeEffect = Fx.none;
-c.speed = 5;
-c.lifetime = 15;
-c.damage = 999999999;
-
-const p = extend(BasicBulletType,{});//LiquidBulletType
-//p.liquid = Vars.content.getByName(ContentType.liquid, "cheat-x250")
-p.shootEffect = Fx.none;
-p.smokeEffect = Fx.neoplasmSplat;
-p.speed = 5;
-p.lifetime = 15;
-p.damage = 999999999;
-const laser = extend(LaserBulletType, {
-    damage: 999999999,
-    length: 600,
-    width: 8,
-    lifetime: 60,
-    colors: [Color.valueOf("ff0000"), Color.valueOf("ff5555"), Color.valueOf("ffaaaa")]
-});
-
-const turret = extend(PowerTurret, "laser-turret", {
-    init() {
-        this.ammo(
-            laser
+        wispion.buildType = prov(() => extend(LaserTurret.LaserTurretBuild, wispion, {}));
+        wispion.requirements = ItemStack.with(
+            Items.copper, 1200,
+            Items.lead, 350,
+            Items.graphite, 300,
+            Items.surgeAlloy, 325,
+            Items.silicon, 325
         );
-        this.super$init();
-    },
-    icons() {
-        return [
-            Core.atlas.find("block-3"),
-            Core.atlas.find(this.name)
-        ];
-    }
-});
-
-turret.buildType = prov(() => extend(PowerTurret.PowerTurretBuild, turret, {}));
-turret.itemCapacity = 0;
-turret.range = 600;
-turret.reload = 1;
-turret.shots = 1;
-turret.health = 900000000;
-turret.powerUse = 10;
-energyBall.fragBullets = 10;
-energyBall.fragBullet = c;
-p.fragBullets = 16;
-c.fragBullet = p;
-
-const turret = extend(ItemTurret,"turret",{
- init(){
-  this.ammo(
-    Vars.content.getByName(ContentType.item, "copper"), energyBall,
-  );
-  this.super$init();
- },
- icons(){
-  return [
-  Core.atlas.find("block-3"),
-  Core.atlas.find(this.name)
-  ];
- }
-});
-turret.buildType = prov(()=>extend(ItemTurret.ItemTurretBuild,turret,{
-}));
-turret.itemCapacity=100000000;
-turret.range = 600;
-turret.reload = 1;
-turret.shots = 2;
-turret.health = 900000000;
+        wispion.shootEffect = Fx.shootBigSmoke2;
+        wispion.shootCone = 40;
+        wispion.recoil = 4;
+        wispion.size = 4;
+        wispion.shake = 2;
+        wispion.range = 195;
+        wispion.reload = 90;
+        wispion.firingMoveFract = 0.5;
+        wispion.shootDuration = 230;
+        wispion.shootSound = Sounds.laserbig;
+        wispion.loopSound = Sounds.beam;
+        wispion.loopSoundVolume = 2;
+        wispion.envEnabled |= Env.space;
+        wispion.scaledHealth = 200;
+        wispion.coolant = consumeCoolant(0.5);
+        wispion.consumePower(17);
